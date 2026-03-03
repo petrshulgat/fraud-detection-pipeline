82ffe39a5998d530553e485e0e3c6e2a158c7435
Petr Shulgat <petrshulgat@mailbox.unideb.hu>
2026-03-04 00:16:25 +0100

    Create create_tables.sql and create_staging.sql

---
+++ ecommerce-data-warehouse/staging/create_staging.sql
@@ -0,0 +1,25 @@
+create table staging.customers(
+    customer_id integer,
+    first_name varchar(100),
+    last_name varchar(100),
+    email varchar(100),
+    country varchar(100),
+    created_at TIMESTAMP
+);
+
+create table staging.products(
+    product_id integer,
+    product_name varchar(200),
+    category varchar(200),
+    price decimal(10, 2)
+);
+
+create table staging.orders(
+    order_id integer,
+    customer_id integer,
+    product_id integer,
+    order_date TIMESTAMP,
+    quantity INTEGER,
+    unit_price decimal(10, 2),
+    total_amount decimal(10, 2)
+);
