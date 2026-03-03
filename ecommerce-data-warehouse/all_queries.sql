269c87a8a780a88637e198cf672234669c2b37d2
Petr Shulgat <petrshulgat@mailbox.unideb.hu>
2026-03-03 23:45:11 +0100

    created tables in staging schema

--- ecommerce-data-warehouse/all_queries.sql
+++ ecommerce-data-warehouse/all_queries.sql
@@ -42,3 +42,29 @@ create table warehouse.fact_orders(
     foreign key (product_key) REFERENCES warehouse.dim_product(product_key),
     foreign key (date_key) REFERENCES warehouse.dim_date(date_key)
 );
+
+create table staging.customers(
+    customer_id integer,
+    first_name varchar(100),
+    last_name varchar(100),
+    email varchar(100),
+    country varchar(100),
+    created_at TIMESTAMP
+);
+
+create table staging.products(
+    product_id integer,
+    product_name varchar(200),
+    category varchar(200),
+    price decimal(10, 2)
+);
+
+create table staging.orders(
+    order_id integer,
+    customer_id integer,
+    product_id integer,
+    order_date TIMESTAMP,
+    quantity INTEGER,
+    unit_price decimal(10, 2),
+    total_amount decimal(10, 2)
+);
