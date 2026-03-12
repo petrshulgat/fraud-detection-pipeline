fe4054a0443e757c2956faf47596c6eea46461f5
Petr Shulgat <petrshulgat@mailbox.unideb.hu>
2026-03-12 23:29:15 +0100

    update consumer for psql

--- fraud-detection-pipeline/consumer/fraud_detector.py
+++ fraud-detection-pipeline/consumer/fraud_detector.py
@@ -1,6 +1,17 @@
 from kafka import KafkaConsumer
+import psycopg2
 import json
 
+# Connect to PostgreSQL
+conn = psycopg2.connect(
+    host="localhost",
+    database="fraud_db",
+    user="admin",
+    password="admin"
+)
+cursor = conn.cursor()
+
+# Connect to Kafka
 consumer = KafkaConsumer(
     "transactions",
     bootstrap_servers="localhost:9092",
@@ -11,11 +22,27 @@ consumer = KafkaConsumer(
 for message in consumer:
     transaction = message.value
 
+    # skip invalid messages
     if "amount" not in transaction:
         print("Skipping invalid message:", transaction)
         continue
 
+    # Fraud detection rule
     if transaction["amount"] > 1000:
-        print("Fraud Alert:", transaction)
+        print("FRAUD ALERT:", transaction)
+
+        # Insert into database
+        cursor.execute("""
+            INSERT INTO fraud_alerts (user_id, amount, merchant, country)
+            VALUES (%s, %s, %s, %s)
+        """, (
+            transaction["user_id"],
+            transaction["amount"],
+            transaction["merchant"],
+            transaction["country"]
+        ))
+
+        conn.commit()
+
     else:
         print("Normal transaction:", transaction)
