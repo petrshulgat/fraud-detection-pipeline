74439b5c98b3740c4673b28e71256455396ac6f1
Petr Shulgat <petrshulgat@mailbox.unideb.hu>
2026-03-14 22:43:43 +0100

    small changes inside consumer

--- fraud-detection-pipeline/consumer/fraud_detector.py
+++ fraud-detection-pipeline/consumer/fraud_detector.py
@@ -1,48 +1,43 @@
 from kafka import KafkaConsumer
 import psycopg2
-import json
+import json 
 
-# Connect to PostgreSQL
 conn = psycopg2.connect(
-    host="localhost",
-    database="fraud_db",
-    user="admin",
-    password="admin"
+    host = 'localhost',
+    database = 'fraud_db',
+    user = 'admin',
+    password = 'admin'
 )
 cursor = conn.cursor()
 
-# Connect to Kafka
 consumer = KafkaConsumer(
-    "transactions",
-    bootstrap_servers="localhost:9092",
-    value_deserializer=lambda x: json.loads(x.decode("utf-8")),
-    auto_offset_reset="earliest"
+    'transactions',
+    bootstrap_servers = 'localhosr:9092',
+    value_deserializer = lambda x: json.loads(x.decode('utf-8')),
+    auto_offset_reset = 'earliest'
 )
 
 for message in consumer:
     transaction = message.value
 
-    # skip invalid messages
-    if "amount" not in transaction:
-        print("Skipping invalid message:", transaction)
+    if 'amount' not in transaction:
+        print('Skip invalid message:', transaction)
         continue
 
-    # Fraud detection rule
-    if transaction["amount"] > 1000:
-        print("FRAUD ALERT:", transaction)
+    if transaction['amount'] > 1000:
+        print('Fraud Alert:', transaction)
 
-        # Insert into database
         cursor.execute("""
-            INSERT INTO fraud_alerts (user_id, amount, merchant, country)
-            VALUES (%s, %s, %s, %s)
+            insert into fraud_alerts(user_id, amount, merchant, country)
+            values(%s, %s, %s, %s)
         """, (
-            transaction["user_id"],
-            transaction["amount"],
-            transaction["merchant"],
-            transaction["country"]
-        ))
+            transaction['user_id'], transaction['amount'], transaction['merchant'], transaction['country']
+        )
+        )
 
         conn.commit()
 
     else:
-        print("Normal transaction:", transaction)
+        print('Normal transaction:', transaction)
+
+    
